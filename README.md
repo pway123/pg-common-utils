9e654aa90639d3330d3a2f7b0327831d2e1fdc3b
John <johnlee@estl.sg>
2022-04-18 16:35:23 +0800

    [PG-3549][John][Adding readme and comment to the newRegexToCheckDomain regex TLD]

---
+++ README.md
@@ -0,0 +1,8 @@
+# Publishing NPM lib
+1. Update package.json version
+2. Commit to github and tag with the version
+3. ``npm login``
+4. ``npm run build``
+5. ``npm publish``
+
+NPM package path: https://www.npmjs.com/package/@pgateway/common-utils 
--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -33,7 +33,7 @@ export function isValidEmail(emailStr: string): boolean {
   const domainPat = new RegExp('^' + atom + '(\\.' + atom + ')*$');
 
   /* The following pattern describes the validity of the domain Eg. should not include
-    special character in the email domain. */
+    special character in the email domain. https://wiki.mozilla.org/TLD_List */
   const newRegexToCheckDomain = /^.+@([a-zA-Z0-9][-a-zA-Z0-9]*(\.[-a-zA-Z0-9]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-zA-Z][a-zA-Z]))?$/;
 
   /* Finally, let's start trying to figure out if the supplied address is
