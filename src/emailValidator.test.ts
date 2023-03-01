8d7c6ac533ac1470b17e842a803a50681ead984a
Eugene <>
2023-03-01 16:51:53 +0800

    [PG-4117][EA][Mobile] To Enhance Email Validation in PG App Contact Details
    - added .dev to TDL
    - update test for valid .dev email
    - bump version number 1.0.3

--- src/emailValidator.test.ts
+++ src/emailValidator.test.ts
@@ -1,7 +1,7 @@
 import { isValidEmail } from './emailValidator';
 
 describe('emailValidator', () => {
-    let scEmailValidator;
+  let scEmailValidator;
   let alphanumericStringValidator;
   let contactNumStringValidator;
   let numericStringValidator;
@@ -24,7 +24,8 @@ describe('emailValidator', () => {
       'test@xn--example.COM',
       'test@isce2.astar.edu.sg',
       'test@example.example.iana.org',
-      '1234567890123456789012345678901234567890123456789012345678901234@iana.org'
+      '1234567890123456789012345678901234567890123456789012345678901234@iana.org',
+      'JiaBabu.ey@me.dev'
     ];
 
     expect.assertions(validEmail.length);
@@ -37,26 +38,26 @@ describe('emailValidator', () => {
 
   test('To test validator to return false when provided a invalid email addresses', (done) => {
     let validEmail = [
-    'apple@go_gmail.com',
-    'apple',
-    'Pepe@gmail.o',
-    'Penguingmail2.com',
-    'google.com.sg',
-    'testing@gmail_.com',
-    'testing@gmail$.com',
-    'testing12345@gm@ail.com',
-    'testing12345@gmail.com.123',
-    '"first"last"@iana.org',
-    'test@123.123.123.123]',
-    'a@[256.123.123.123]',
-    'gatsby@f.sc.ot.t.f.i.tzg.era.l.d.',
-    'first.last@sub.do,com',
-    'Jia.Babuey@gmail.aaaaaaaaa',
+      'apple@go_gmail.com',
+      'apple',
+      'Pepe@gmail.o',
+      'Penguingmail2.com',
+      'google.com.sg',
+      'testing@gmail_.com',
+      'testing@gmail$.com',
+      'testing12345@gm@ail.com',
+      'testing12345@gmail.com.123',
+      '"first"last"@iana.org',
+      'test@123.123.123.123]',
+      'a@[256.123.123.123]',
+      'gatsby@f.sc.ot.t.f.i.tzg.era.l.d.',
+      'first.last@sub.do,com',
+      'Jia.Babuey@gmail.aaaaaaaaa'
     ];
 
     expect.assertions(validEmail.length);
     for (var index in validEmail) {
-        let testResult = isValidEmail(validEmail[index]);
+      let testResult = isValidEmail(validEmail[index]);
       expect(testResult).toBe(false);
       done();
     }
--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -34,7 +34,7 @@ export function isValidEmail(emailStr: string): boolean {
 
   /* The following pattern describes the validity of the domain Eg. should not include
     special character in the email domain. https://wiki.mozilla.org/TLD_List */
-  const newRegexToCheckDomain = /^.+@([a-zA-Z0-9][-a-zA-Z0-9]*(\.[-a-zA-Z0-9]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-zA-Z][a-zA-Z]))?$/;
+  const newRegexToCheckDomain = /^.+@([a-zA-Z0-9][-a-zA-Z0-9]*(\.[-a-zA-Z0-9]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|dev|[a-zA-Z][a-zA-Z]))?$/;
 
   /* Finally, let's start trying to figure out if the supplied address is
        valid. */
